f785d506abdd35eda0876907b5bf0c1da796831d
Rodrigo Salmeron <fobos.salmeron@gmail.com>
2021-05-18 19:30:12 -0500

    config updated

--- config.js
+++ config.js
@@ -1,99 +1,78 @@
 var klaroConfig = {
-
-    version: 1,
-
-    elementID: 'klaro',
-
-    noAutoLoad: false,
-
-    htmlTexts: true,
-
-    embedded: false,
-
-    groupByPurpose: true,
-
-    storageMethod: 'cookie',
-
-    cookieName: 'klaro',
-
-    cookieExpiresAfterDays: 120,
-
-    default: false,
-
-    mustConsent: true,
-
-    acceptAll: true,
-
-    hideDeclineAll: false,
-
-    hideLearnMore: false,
-
-    noticeAsModal: false,
-
-    translations: {
-        zz: {
-            privacyPolicyUrl: 'URL',
-        },
-        de: {
-            consentModal: {
-                title: '<h4>Services, die wir verwenden möchten</h4>',
-                description:
-                    'Hier können Sie einsehen und anpassen, welche Information wir über Sie sammeln.',
-            },
-            googleAnalytics: {
-                description: 'Sammeln von Besucherstatistiken',
-            },
-            purposes: {
-                analytics: 'Besucher-Statistiken',
-            },
-        },
-        en: {
-            consentModal: {
-                title: '<h4>Services we would like to use</h4>',
-                description:
-                    'Here you can see and customize the information that we collect about you.',
-            },
-            googleAnalytics: {
-                description: 'Collecting of visitor statistics',
-            },
-            purposes: {
-                analytics: 'Analytics',
-            },
-        },
+  version: 1,
+  elementID: "klaro",
+  noAutoLoad: false,
+  htmlTexts: true,
+  embedded: false,
+  groupByPurpose: true,
+  storageMethod: "cookie",
+  cookieName: "blockstem",
+  cookieExpiresAfterDays: 120,
+  default: false,
+  mustConsent: true,
+  acceptAll: true,
+  hideDeclineAll: true,
+  hideLearnMore: false,
+  noticeAsModal: false,
+  disablePoweredBy: true,
+  translations: {
+    zz: {
+      privacyPolicyUrl: "/privacidad",
     },
-
-    services: [
-        {
-            name: 'googleAnalytics', 
-            title: 'Google Analytics',
-            purposes: ['analytics'],
-            cookies: [
-                [/^_g.*/, '/', '.webflow.io'], // delete already set cookies at loading time of Klaro
-            ],
-            optOut: false,
-            callback: function(consent, service) {
-                console.log(
-                    'User consent for service ' + service.name + ': consent=' + consent
-                );
-            },
-        },
-        {
-            name: 'facebook', 
-            title: 'Facebook',
-            purposes: ['marketing'],
-            cookies: [
-                [/^_fb.*/, '/', '.webflow.io'], // delete already set cookies at loading time of Klaro
-            ],
-            optOut: false,
-        },
-        {
-            name: 'hubspot', 
-            title: 'Hacebook',
-            purposes: ['marketing'],
-            cookies: [
-                [/^_hs.*/, '/', '.webflow.io'], // delete already set cookies at loading time of Klaro
-            ],
-            optOut: false,
-        },
-    ],
+    es: {
+      consentModal: {
+        title: "<h4>Quisieramos usar</h4>",
+        description:
+          "Aquí puedes personalizar la información que recolectamos.",
+      },
+      privacyPolicyUrl: "/privacidad",
+      googleAnalytics: {
+        description: "Estadísticas de uso del sitio.",
+      },
+      facebook: {
+        description: "Información para seguimiento de Facebook, Inc.",
+      },
+      hubspot: {
+        description: "Información para seguimiento de Hubspot, Inc.",
+      },
+      purposes: {
+        analytics: "Analytics",
+        marketing: "Publiidad",
+      },
+    },
+  },
+  services: [
+    {
+      name: "googleAnalytics",
+      title: "Google Analytics",
+      purposes: ["analytics"],
+      cookies: [
+        [/^_g.*/, "/", ".webflow.io"], // delete already set cookies at loading time of Klaro
+      ],
+      optOut: false,
+      callback: function (consent, service) {
+        console.log(
+          "User consent for service " + service.name + ": consent=" + consent
+        );
+      },
+    },
+    {
+      name: "facebook",
+      title: "Facebook",
+      purposes: ["marketing"],
+      cookies: [
+        [/^_fb.*/, "/", ".webflow.io"], // delete already set cookies at loading time of Klaro
+      ],
+      optOut: false,
+    },
+    {
+      name: "hubspot",
+      title: "Hacebook",
+      purposes: ["marketing"],
+      cookies: [
+        [/^_hs.*/, "/", ".webflow.io"], // delete already set cookies at loading time of Klaro
+      ],
+      optOut: false,
+    },
+  ],
 };
